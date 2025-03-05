247c94723c4f0b68207239d28936df9354623c24
arbennett <bennett.andr@gmail.com>
2025-03-05 15:53:07 -0500

    Remove cruft

--- emulator_configurable/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import model_builder
-from . import inference
-from . import models
-from . import data_loader
-from . import train
-from . import utils
-from . import main
-
-__version__  = '0.0.1'
