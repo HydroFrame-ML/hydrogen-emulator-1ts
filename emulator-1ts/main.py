236dcb1f73877128bb415b5aa3424e12cfb2f18d
arbennett <bennett.andr@gmail.com>
2025-03-10 14:20:51 -0400

    Stub out test function

--- emulator-1ts/main.py
+++ emulator-1ts/main.py
@@ -1,11 +1,12 @@
 import yaml
 import torch
+import pandas as pd
 
 from dataset import ParFlowDataset
 from model import get_model
 from train import train_model
 from argparse import ArgumentParser
-from utils import get_optimizer, get_loss, get_dtype
+from utils import get_optimizer, get_loss, get_dtype, calculate_metrics
 from torch.utils.data import DataLoader
 
 def read_config(config_path):
@@ -41,6 +42,7 @@ def train(
     device: str,
     num_workers: int,
     dtype: str,
+    config: dict,
     **kwargs
 ):
     # Create the data loader
@@ -53,7 +55,6 @@ def train(
         shuffle=True, 
         num_workers=num_workers
     )
-
 
     # Create the model
     # Add names of model inputs to model definition for scaling, if needed
@@ -81,7 +82,13 @@ def train(
     metrics_filename = f'{log_location}/{name}_metrics.csv'
     weights_filename = f'{log_location}/{name}_weights_only.pth'
     model_filename = f'{log_location}/{name}_model.pth'
+    config['model_path'] = model_filename
+    config['weights_path'] = weights_filename
+    config['metrics_path'] = metrics_filename
+    with open(f'{log_location}/{name}_config.yaml', 'w') as f:
+        yaml.safe_dump(config, f)
     metrics.to_csv(metrics_filename)
+    model = model.to(torch.float64)
     torch.save(model.state_dict(), weights_filename)
     m = torch.jit.script(model)
     torch.jit.save(m, model_filename)
@@ -89,20 +96,74 @@ def train(
     print('----------------------------------------')
     print(f'Metrics saved to {metrics_filename}')
     print(f'Model saved to {model_filename}')
+    print(f'Config saved to {log_location}/{name}_config.yaml')
 
 
-def test():
-    pass
+
+def test(
+    name: str,
+    log_location: str,
+    model_path: str,
+    data_def: dict,
+    batch_size: int,
+    device: str,
+    num_workers: int,
+    dtype: str,
+    **kwargs
+):
+    dtype = get_dtype(dtype)
+    # Load the model
+    model = torch.jit.load(model_path)
+    model = model.to(device).to(dtype)
+    
+    # Create the data loader
+    dataset = ParFlowDataset(**data_def, dtype=dtype)
+    test_dl = DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        collate_fn=custom_collate, 
+        shuffle=False, 
+        num_workers=num_workers
+    )
+    
+    model.eval()
+    all_outputs = []
+    all_targets = []
+    
+    with torch.no_grad():
+        for batch in test_dl:
+            s, e, p, y = batch
+            s, e, p, y = s.to(device), e.to(device), p.to(device), y.to(device)
+            outputs = model(s, e, p)
+            all_outputs.append(outputs.cpu())
+            all_targets.append(y.cpu())
+    
+    all_outputs = torch.cat(all_outputs)
+    all_targets = torch.cat(all_targets)
+    print(f'All outputs shape: {all_outputs.shape}')
+    print(f'All targets shape: {all_targets.shape}')
+    
+    # Save the outputs
+    output_filename = f'{log_location}/{name}_outputs.pt'
+    torch.save(all_outputs, output_filename)
+    print(f'Outputs saved to {output_filename}')
+    
+    # Calculate and print metrics
+    metrics = calculate_metrics(all_outputs, all_targets)
+    metrics_filename = f'{log_location}/{name}_test_metrics.csv'
+    metrics.to_csv(metrics_filename)
+    print(f'Test metrics saved to {metrics_filename}')
+    print(metrics)
+
 
 def main(config, mode):
     config = read_config(config)
 
     if mode == "train":
         print("TRAINING")
-        train(**config)
+        train(**config, config=config)
     elif mode == "test":
         print("TESTING")
-        # Note: Not implemented
         test(**config)
 
 
